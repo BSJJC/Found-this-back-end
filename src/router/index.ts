c86ff436c8d691bafc2fe92054b5188e076ae99d
BSJJC <805086021@qq.com>
2023-02-16 13:14:53 +0800

    start: add form check

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,20 @@
+import TheTest from '@/views/TheTest.vue';
 import LogInVue from '@/views/LogIn.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    /// TODO 
     {
       path: "/",
       name: "LogIn",
       component: LogInVue,
-    }
+    },
+    {
+      path: "/test",
+      name: "theTest",
+      component: TheTest,
+    },
   ]
 })
 
