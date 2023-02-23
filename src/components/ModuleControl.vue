9a0ba251f883fef7483d05898ebd85f4993979e1
BSJJC <805086021@qq.com>
2023-02-23 15:45:25 +0800

    done: module control area

--- src/components/ModuleControl.vue
+++ src/components/ModuleControl.vue
@@ -1,5 +1,29 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-red-300 w-full h-full mt-4 pr-4"></div>
+  <div class="w-full h-full pt-4 flex flex-row justify-between">
+    <!-- module edit area -->
+    <div class="w-1/2 h-full flex justify-start items-center">
+      <i-ep-Document class="mr-5 text-xl" />
+      <i-ep-Delete class="mr-5 text-xl" />
+    </div>
+    <!-- module add button -->
+    <div>
+      <el-button class="w-20">添加</el-button>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.el-button {
+  background-color: #3785ea;
+  color: white;
+  border: none;
+  font-size: bold;
+
+  &:hover {
+    background-color: #3785ea;
+    color: rgb(227, 227, 227);
+  }
+}
+</style>
