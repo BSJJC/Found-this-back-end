2049993650345859ae5627b7d3315490e104e7b5
BSJJC <805086021@qq.com>
2023-03-01 15:15:38 +0800

    feat: module list fade animation

--- src/components/ModuleList.vue
+++ src/components/ModuleList.vue
@@ -31,7 +31,9 @@ function removeFromSelecteds(index: number) {
  *  add of remove an object into or from selected moduels
  * @param index the index of te selected item
  */
-function addOrRemove(index: number) {
+function addOrRemove(_index: { title: string; intro: string }) {
+  const index = listData.value.indexOf(_index);
+
   if (selectedModuleIndexes.value.indexOf(index) === -1) {
     // not in selected indexes
     addToSelecteds(index);
@@ -40,32 +42,37 @@ function addOrRemove(index: number) {
     removeFromSelecteds(index);
   }
 }
+
+const test = ref([1, 2, 3, 4, 5]);
+
+setTimeout(() => {
+  test.value.splice(2, 1);
+}, 2000);
 </script>
 
 <template>
   <div class="w-full h-full pt-0">
     <div class="w-full h-full p-4 pt-0">
       <el-scrollbar class="pr-3">
-        <el-checkbox-group
-          v-model="checkboxGroup"
-          class="flex flex-col justify-center items-center w-full h-full pl-2"
-        >
-          <el-checkbox
-            v-for="(i, index) in listData"
-            :key="index"
-            :label="i.title"
-            @change="addOrRemove(index)"
-            class="w-full pl-3 mb-2 border-2 border-gray-200 rounded-lg"
-          >
-            <div class="w-full flex flex-row text-xl">
-              <div class="w-64 ml-4 truncate transition duration-200">
-                {{ i.title }}
+        <el-checkbox-group v-model="checkboxGroup">
+          <transition-group name="fade-right">
+            <el-checkbox
+              v-for="item in listData"
+              :key="item"
+              :label="item.title"
+              @change="addOrRemove(item)"
+              class="w-full pl-3 mb-2 border-2 border-gray-200 rounded-lg"
+            >
+              <div class="flex flex-row text-xl">
+                <div class="w-64 ml-4 truncate transition duration-200">
+                  {{ item.title }}
+                </div>
+                <div class="w-full ml-10 truncate transition duration-200">
+                  {{ item.intro }}
+                </div>
               </div>
-              <div class="w-full ml-10 truncate transition duration-200">
-                {{ i.intro }}
-              </div>
-            </div>
-          </el-checkbox>
+            </el-checkbox>
+          </transition-group>
         </el-checkbox-group>
       </el-scrollbar>
     </div>
@@ -84,5 +91,23 @@ function addOrRemove(index: number) {
 
 .el-checkbox__label {
   width: 100%;
+}
+
+.fade-right-move,
+.fade-right-enter-active,
+.fade-right-leave-active {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.fade-right-enter-from,
+.fade-right-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.fade-right-enter-to,
+.fade-right-leave-from {
+  opacity: 1;
+  transform: translateX(0px);
 }
 </style>
